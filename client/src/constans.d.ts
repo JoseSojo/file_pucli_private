40bec2b9f30f435e49d1b77cd92860c211970d23
JoseSojo <jsojo346@gmail.com>
2024-01-09 11:43:26 -0400

    test 1

--- client/src/constans.d.ts
+++ client/src/constans.d.ts
@@ -1,2 +1,5 @@
 
-export const URL = 'http://localhost:30456/'
+// http://localhost:30456/
+// https://api-files-public-private.onrender.com
+//
+export const URL = 'https://api-files-public-private.onrender.com/'
