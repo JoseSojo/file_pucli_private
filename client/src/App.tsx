58511820fefe4970f4fd859ff728f3ff3a003eda
JoseSojo <jsojo346@gmail.com>
2024-01-09 14:47:29 -0400

    fix(router): bye bye react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,28 +6,41 @@ import { ContactAdmin } from "./pages/ContactAdmin";
 import { AdminPage } from "./pages/private/AdminPage";
 import { DirectPage } from "./pages/private/DirectPage";
 import { TeacherPage } from "./pages/private/TeacherPage";
-import { ValidSession } from "./pages/ValidSession";
+import { useEffect, useState } from "react";
+import { NAVIGATION_EVENT } from "./constans";
 
 function App() {
-  // const auth = useAuth();
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+  const [renderApp, setRenderApp] = useState(false);
+
+  useEffect(()=> {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname);
+    }
+
+    window.addEventListener(NAVIGATION_EVENT, onLocationChange);
+    window.addEventListener('popstate', onLocationChange);
+    setRenderApp(!renderApp);
+    return () => {
+      window.removeEventListener(NAVIGATION_EVENT, onLocationChange);
+      window.removeEventListener('popstate', onLocationChange);
+    }
+
+  }, [])
+
 
   return (
-    <Layout>
-      <BrowserRouter>
-        <Routes>
-
-          <Route path='/login' Component={LoginPage} />
-          <Route path='/contact/admin' Component={ContactAdmin} />
-
-          <Route element={<ValidSession />}>
-            <Route path='/dashboard/direct' Component={DirectPage} />
-            <Route path='/dashboard/admin' Component={AdminPage} />
-            <Route path='/dashboard/teacher' Component={TeacherPage} />
-          </Route>
-
-        </Routes>
-      </BrowserRouter>
-    </Layout>
+    <>
+      <Layout>
+        { currentPath == '/login' && <LoginPage /> }
+        { currentPath == '/contact/admin' && <ContactAdmin />  }         
+      </Layout>
+      <>
+        { currentPath == '/dashboard/direct' && <DirectPage /> }
+        { currentPath == '/dashboard/admin' && <AdminPage /> }
+        { currentPath == '/dashboard/teacher' && <TeacherPage /> }
+      </>
+    </>
   )
 }
 
--- client/src/constans.d.ts
+++ client/src/constans.d.ts
@@ -2,4 +2,5 @@
 // http://localhost:30456/
 // https://api-files-public-private.onrender.com
 //
-export const URL = 'https://api-files-public-private.onrender.com/'
+export const URL = 'https://api-files-public-private.onrender.com/';
+export const NAVIGATION_EVENT = 'pushstate';
