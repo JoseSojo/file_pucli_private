606dd79f2495ebd78a5bb40589e52f5d68276809
JoseSojo <jsojo346@gmail.com>
2024-01-21 13:34:48 -0400

    fix(styles): styles in te index

--- client/src/pages/IndexPage.tsx
+++ client/src/pages/IndexPage.tsx
@@ -36,7 +36,7 @@ export const IndexPage = () => {
             </header>
             <main className='flex flex-col gap-y-5 justify-center items-center'>
 
-                <section className='grid w-[90%] md:w-[70%] place-items-center h-screen'>
+                <section className='grid w-[90%] md:w-[70%] place-items-center min-h-screen'>
                     <div className='grid grid-cols-1 lg:grid-cols-[.75fr_1fr] w-full'>
                         <img src={IMG1} className='w-full h-[450px] object-cover rounded-[50%_50%_0_0] lg:rounded-[50%_0_0_50%]' />
                         <div className='bg-white lg:rounded-r-xl shadow w-full h-[450px] p-5 lg:p-10 flex justify-center items-center flex-col'>
@@ -56,13 +56,13 @@ export const IndexPage = () => {
                     </div>
                 </section>
 
-                <section className='flex flex-col justify-center items-center w-[90%] md:w-[70%] place-items-center h-screen'>
+                <section className='flex flex-col justify-center items-center w-[90%] md:w-[70%] place-items-center min-h-screen'>
                     <h3 className='font-bebas text-6xl text-gray-700'>Galeria Interactiva De La Institución</h3>
                     <button className='bg-amber-500 hover:bg-amber-300 duration-200 px-8 py-2 rounded-md font-bold text-white my-5'>Ver más</button>
                     <Carrousel />
                 </section>
 
-                <section className='flex flex-col justify-center items-center w-[90%] md:w-[70%] place-items-center h-screen'>
+                <section className='flex flex-col justify-center items-center w-[90%] md:w-[70%] place-items-center min-h-screen'>
                     <h3 className='font-bebas text-6xl text-gray-700'>Nuestros Valores</h3>
                     
                     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
@@ -98,7 +98,7 @@ export const IndexPage = () => {
 
                 </section>
 
-                <section className='grid w-[90%] md:w-[70%] place-items-center h-screen'>
+                <section className='grid w-[90%] md:w-[70%] place-items-center min-h-screen'>
                     <div className='grid grid-cols-1 lg:grid-cols-[.75fr_1fr] w-full'>
                         <img src={IMG3} className='w-full h-[450px] object-cover rounded-[50%_50%_0_0] lg:rounded-[50%_0_0_50%]' />
                         <div className='bg-white lg:rounded-r-xl shadow w-full h-[450px] p-5 lg:p-10 flex justify-center items-center flex-col'>
